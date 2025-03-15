8f15d51ae923b523183450695bc6f1f80a1ba372
Abush Natanem <abushnatanem@gmail.com>
2025-03-15 16:07:49 +0300

    addd the clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div></div>;
+  console.log("Something is rendered on the server side");
+  return (
+    <div className="w-full h-screen bg-gray-800">
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 20,
+        }}
+      >
+        <h1>My App</h1>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </header>
+    </div>
+  );
 }
