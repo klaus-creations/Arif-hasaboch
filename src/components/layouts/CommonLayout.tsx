8ea17b812b86c3734afdd1569773b6f70e9675c5
Abush Natanem <abushnatanem@gmail.com>
2025-03-16 21:03:07 +0300

    add layout

---
+++ src/components/layouts/CommonLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../common/Header";
+
+export default function CommonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="w-[95%] sm:w-[90%] lg:w-[85%] 2xl:w-[80%] mx-auto h-screen flex flex-col  overflow-hidden">
+      <div className="w-full h-[10%]">
+        <Header />
+      </div>
+      <div className="w-full h-[90%] overflow-y-auto">{children}</div>
+    </div>
+  );
+}
