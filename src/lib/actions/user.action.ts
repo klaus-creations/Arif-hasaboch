12e5cba8ba2de274a0820f39945a8edbac48a6e4
Abush Natanem <abushnatanem@gmail.com>
2025-03-18 14:10:37 +0300

    add additional user actions

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -2,6 +2,9 @@
 
 import connectDB from "@/config/db";
 import userModel from "@/models/user.model";
+import { TCreateUser, TDeleteUser, TUpdateUser } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import thoughtsModel from "@/models/thoughts.model";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getUserById = async function (data: any) {
@@ -16,3 +19,65 @@ export const getUserById = async function (data: any) {
     console.log(error);
   }
 };
+
+export async function createUser(userData: TCreateUser) {
+  try {
+    await connectDB();
+
+    const newUser = await userModel.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(updateDatas: TUpdateUser) {
+  try {
+    await connectDB();
+
+    const { clerkId, updateData, path } = updateDatas;
+
+    await userModel.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: TDeleteUser) {
+  try {
+    await connectDB();
+
+    const { clerkId } = params;
+
+    const user = await userModel.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Delete user from database
+    // and questions, answers, comments, etc.
+
+    // get user question ids
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+
+    // delete user thoughts
+    await thoughtsModel.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
+
+    const deletedUser = await userModel.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
