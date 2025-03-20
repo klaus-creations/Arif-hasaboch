a1e01672b91c509c10e2f112c8ad4214e9c4df7b
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 20:50:51 +0300

    update

--- src/app/(root)/tags/[id]/page.tsx
+++ src/app/(root)/tags/[id]/page.tsx
@@ -2,5 +2,6 @@ import React from "react";
 
 export default async function page({ params }: { params: { id: string } }) {
   const { id } = await params;
+  console.log(id);
   return <div className="size-full px-5 py-1"></div>;
 }
