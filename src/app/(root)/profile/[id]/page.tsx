0fed43ea2c7537a5da8e30c6d7570e3d0f08f554
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 21:21:35 +0300

    update params with promises

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -5,18 +5,22 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const authData = await auth();
 
   if (!authData?.userId) redirect("/");
 
-  const user = await getUserById({ userId: id.id });
+  const user = await getUserById({ userId: id });
   if (!user) redirect("/");
 
   return (
     <div className="size-full overflow-y-auto px-5 py-1">
-      {id.id === authData.userId ? (
+      {id === authData.userId ? (
         <SelfProfile data={JSON.stringify(user)} />
       ) : (
         <OthersProfile data={JSON.stringify(user)} />
--- src/app/(root)/thoughts/[id]/page.tsx
+++ src/app/(root)/thoughts/[id]/page.tsx
@@ -1,7 +1,11 @@
 import Detail from "@/components/Details/Detail";
 import React from "react";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
 
   return (
