de12fd2e38964b821b4e81cbad520ae485519cee
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 21:32:18 +0300

    change

--- src/app/(root)/tags/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default async function page({ params }: { params: { id: string } }) {
-  const { id } = await params;
-  console.log(id);
-  return <div className="size-full px-5 py-1"></div>;
-}
--- src/app/(root)/tags/page.tsx
+++ src/app/(root)/tags/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import Tags from "@/components/Tags/Tags";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  console.log(id);
+export default async function Page() {
   return (
     <div className="size-full">
       <Tags />
