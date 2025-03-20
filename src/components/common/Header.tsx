c241add4ced11ad04f0fa78ac694ecfbe20481d1
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 22:24:28 +0300

    update header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,7 +10,6 @@ import React from "react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -67,14 +66,43 @@ const SheetTriggerC = function () {
         <SheetTrigger>
           <Menu className="size-5" />
         </SheetTrigger>
-        <SheetContent className="bg-[inherit] border-[inherit]">
+        <SheetContent className="bg-[inherit] border-[inherit] flex flex-col justify-between">
           <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
+            <Link
+              href={"/"}
+              className="text-xl lg:text-2xl font-bol tracking-[1px] text-emerald-500 first-letter:text-2xl first-letter:font-extrabold lg:first-letter:text-3xl"
+            >
+              arif-hasab
+            </Link>
+
+            <nav className="flex flex-col items-center gap-8">
+              <Link href={"/"}>Home</Link>
+              <Link href={"/"}>About</Link>
+              <Link href={"/tags"}>Tags</Link>
+              <Link href={"/members"}>Members</Link>
+            </nav>
           </SheetHeader>
+
+          <SheetTitle>
+            {" "}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <div className="flex items-center gap-2">
+                <SignInButton>
+                  <button className="text-sm lg:text-base font-bold px-2 hover:bg-emerald-600 duration-300  py-1 rounded-md bg-emerald-500">
+                    Sign In
+                  </button>
+                </SignInButton>
+                <SignUpButton>
+                  <button className="text-sm lg:text-base font-bold px-2  py-1 rounded-md border-[1px] border-emerald-500">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </div>
+            </SignedOut>
+          </SheetTitle>
         </SheetContent>
       </Sheet>
     </div>
