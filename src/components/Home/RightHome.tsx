23bc05597828627d87910e2549dda524e191b7f5
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 13:58:58 +0300

    show popular tags

--- src/components/Home/RightHome.tsx
+++ src/components/Home/RightHome.tsx
@@ -1,11 +1,10 @@
 import React from "react";
+import PopularTags from "../Tags/PopularTags";
 
 export default function RightHome() {
   return (
-    <div className="h-full hidden lg:flex lg:w-[25%] overflow-y-auto flex-col items-center">
-      <h2 className="text-base lg:text-xl font-bold tracking-[1px] text-gray-100">
-        Most Popular Tags
-      </h2>
+    <div className="h-full hidden lg:flex lg:w-[25%] overflow-y-auto flex-col items-center py-3">
+      <PopularTags />
     </div>
   );
 }
