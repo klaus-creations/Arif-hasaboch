84147fa8593c8a1091fd5918f22a3013f6dd7db9
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 21:00:25 +0300

    update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,12 +2,13 @@ import LeftHome from "@/components/Home/LeftHome";
 import RightHome from "@/components/Home/RightHome";
 import React from "react";
 
-interface PageProps {
-  searchParams: Record<string, string | string[] | undefined>;
-}
-
-export default function Page({ searchParams }: PageProps) {
-  const data = typeof searchParams.query === "string" ? searchParams.query : "";
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const data =
+    typeof searchParams?.query === "string" ? searchParams.query : "";
 
   console.log(data);
 
