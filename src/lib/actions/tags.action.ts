77be72f1829261dbded898e55537ad960d8951da
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 13:58:33 +0300

    some function for tags

--- src/lib/actions/tags.action.ts
+++ src/lib/actions/tags.action.ts
@@ -3,6 +3,7 @@
 import connectDB from "@/config/db";
 import { TGetAllTags } from "./shared.types";
 import tagModel, { ITag } from "@/models/tags.model";
+import thoughtsModel from "@/models/thoughts.model";
 // import thoughtsModel from "@/models/thoughts.model";
 
 export const getAllTags = async function (
@@ -12,13 +13,45 @@ export const getAllTags = async function (
   try {
     await connectDB();
 
-    const response = await tagModel.find(data);
-    //   .populate({ path: "thoughts", model: thoughtsModel });
-
-    //   .sort({ createdAt: -1 });
+    const response = await tagModel
+      .find(data)
+      .populate({ path: "thoughts", model: thoughtsModel })
+      .sort({ createdAt: -1 });
     return { response };
   } catch (error) {
     console.log(error);
     return { response: [] };
+  }
+};
+
+export const getPopularTags = async function (): Promise<{ response: ITag[] }> {
+  try {
+    await connectDB();
+
+    const response = await tagModel
+      .find()
+      .populate({ path: "thoughts", model: thoughtsModel })
+      .sort({ thoughts: -1 })
+      .limit(10);
+
+    return { response };
+  } catch (error) {
+    console.log(error);
+    return { response: [] };
+  }
+};
+
+export const getTagById = async function (
+  tagId: string
+): Promise<{ response: ITag | null }> {
+  try {
+    await connectDB();
+
+    const response = await tagModel.findById(tagId);
+
+    return { response };
+  } catch (error) {
+    console.log(error);
+    return { response: null };
   }
 };
