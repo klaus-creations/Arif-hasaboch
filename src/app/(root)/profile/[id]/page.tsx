132da2fdaa2ce24795194a502c334640a1435a98
Abush Natanem <abushnatanem@gmail.com>
2025-03-20 20:35:24 +0300

    update

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -6,17 +6,17 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
+  const id = await params;
   const authData = await auth();
 
   if (!authData?.userId) redirect("/");
 
-  const user = await getUserById({ userId: id });
+  const user = await getUserById({ userId: id.id });
   if (!user) redirect("/");
 
   return (
     <div className="size-full overflow-y-auto px-5 py-1">
-      {id === authData.userId ? (
+      {id.id === authData.userId ? (
         <SelfProfile data={JSON.stringify(user)} />
       ) : (
         <OthersProfile data={JSON.stringify(user)} />
