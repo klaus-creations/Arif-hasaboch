b9ec2aaa87fcd9d8e7bf80a5f91d1c130d8b2885
Abush Natanem <abushnatanem@gmail.com>
2025-03-25 21:51:09 +0300

    update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import LeftHome from "@/components/Home/LeftHome";
 import RightHome from "@/components/Home/RightHome";
+import userModel from "@/models/user.model";
 import React from "react";
 
 // Correct Next.js Page Props Type
@@ -10,6 +11,9 @@ export default async function Page({
   searchParams: Promise<{ query: string }>;
 }) {
   const { query } = await searchParams;
+
+  const users = await userModel.find({});
+  console.log(users);
   return (
     <div className="w-full h-full overflow-hidden flex justify-between">
       <LeftHome query={query} />
